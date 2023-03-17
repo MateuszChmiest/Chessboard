673861c46eb7610d7fb4e291f67286681993e3d4
Mateusz Chmiest <mateusz_269@icloud.com>
2023-03-17 21:59:38 +0100

    creating dashboard

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,36 @@
-'use strict';
+const board = document.querySelector(".board");
+const boardSize = document.getElementById("boardSize");
+const dasboardButton = document.querySelector(".dashboard__data--btn");
+const dashboardPanel = document.querySelector(".dashboard__panel");
+console.log(boardSize);
 
-function counter() {
-  let seconds = 0;
-  setInterval(() => {
-    seconds += 1;
-    document.getElementById('app').innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
-  }, 1000);
+dasboardButton.addEventListener("click", (e) => {
+	e.preventDefault();
+  dashboardPanel.classList.add('hidden');
+  board.classList.remove('hidden');
+  createChessBoard();
+	console.log(boardSize.value);
+});
+
+const createChessBoard = () => {
+  const chessBoardTable = document.createElement("table");
+
+  for (let i = 0; i < boardSize.value; i++) {
+    const tr = document.createElement("tr");
+    for (let j = 0; j < boardSize.value; j++) {
+      const td = document.createElement("td");
+  
+      if ((i + j) % 2 == 0) {
+        td.setAttribute("class", "cell whiteCell");
+        tr.appendChild(td);
+      } else {
+        td.setAttribute("class", "cell blackCell");
+        tr.appendChild(td);
+      }
+    }
+    chessBoardTable.appendChild(tr);
+  }
+  board.appendChild(chessBoardTable);
 }
 
-counter();
+
